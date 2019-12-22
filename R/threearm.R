fbba74fa0b13cca5a0350033336a6ecb8fbab18f
Hsin-wen Chang <newschang678@gmail.com>
2019-12-22 20:47:51 +0800

    Update new version of threearm

--- R/threearm.R
+++ R/threearm.R
@@ -1,6 +1,6 @@
 #' Time to first remission data
 #'
-#' @description The data frame \code{threearm} is obtained by perturbing the time-to-remission data 
+#' @description The data frame \code{threearm} is obtained by resampling from perturbed time-to-remission 
 #' from patients in a three-arm randomized clinical trial for the treatment of major depression. 
 #' See \code{\link{nocrossings}}, \code{\link{ptwiseELtest}} and \code{\link{supELtest}} for the application.
 #' @format The \code{threearm} is a data frame with 664 observations of 3 variables,
