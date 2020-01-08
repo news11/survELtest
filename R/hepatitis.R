8b51ef4a4dcfd897f6b576a3e0d794130897e350
Hsin-wen Chang <newschang678@gmail.com>
2020-01-08 14:23:23 +0800

    Update hepatitis.R

--- R/hepatitis.R
+++ R/hepatitis.R
@@ -1,7 +1,7 @@
 #' Severe alcoholic hepatitis data
 #'
 #' @description The data frame \code{hepatitis} is obtained by digitizing the published
-#' Kaplan-Meier curves in Nguyen-Khac et al (2011). The method of digitizing is described in
+#' Kaplan-Meier curves in Nguyen-Khac et al. (2011). The method of digitizing is described in
 #' Guyot et al. (2012).
 #' See \code{\link{intELtest}} for the application.
 #' @format The \code{hepatitis} is a data frame with 174 observations of 3 variables,
@@ -19,7 +19,17 @@
 #' "Enhanced secondary analysis of survival data: reconstructing the data
 #' from published Kaplan-Meier survival curves," \emph{BMC Medical Research Methodology},
 #' 12(1):9. \url{http://bmcmedresmethodol.biomedcentral.com/articles/10.1186/1471-2288-12-9}
-#' 
-#' @seealso \code{\link{intELtest}}
+#' @references
+#' \itemize{
+#'    \item Nguyen-Khac et al., "Glucocorticoids plus N-Acetylcysteine
+#'          in Severe Alcoholic Hepatitis," \emph{The New England Journal of Medicine},
+#'          Vol. 365, No. 19, pp. 1781-1789 (2011).
+#'          \url{http://www.nejm.org/doi/full/10.1056/NEJMoa1101214#t=article}
+#'    \item P. Guyot, A. E. Ades, M. J. N. M. Ouwens, and N. J. Welton,
+#'          "Enhanced secondary analysis of survival data: reconstructing the data
+#'           from published Kaplan-Meier survival curves," \emph{BMC Medical Research Methodology},
+#'          12(1):9. \url{http://bmcmedresmethodol.biomedcentral.com/articles/10.1186/1471-2288-12-9}
+#' }
+#' @seealso \code{\link{intELtest}}, \code{\link{supELtest}}, \code{\link{ptwiseELtest}}, \code{\link{nocrossings}}
 
 "hepatitis"
