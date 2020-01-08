ff8fe423c4b7be56976fd14fd76f9be59c0576fa
Hsin-wen Chang <newschang678@gmail.com>
2020-01-08 14:19:36 +0800

    Update division00.R

--- R/division00.R
+++ R/division00.R
@@ -1,6 +1,6 @@
 
 division00 <- function(x, y) {
-  out <- x/y
-  out[as.logical((x == 0)*(x == y))] <- 1
-  return (out)  
+    out <- x/y
+    out[as.logical((x == 0)*(x == y))] <- 1
+    return (out)
 }
